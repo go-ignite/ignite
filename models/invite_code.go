8ae8187b51d5dc514c2a07f975f286be62d37125
Timothy <yexiaozhou2003@gmail.com>
2017-05-31 15:46:56 +0800

    update invite code entity

--- models/invite_code.go
+++ models/invite_code.go
@@ -6,6 +6,8 @@ type InviteCode struct {
 	Id           int64     `xorm:"pk autoincr notnull"`
 	InviteCode   string    `xorm:"not null"`
 	PackageLimit int       `xorm:"not null"`
+	Available    bool      `xorm:"not null default 1"`
+	UserId       int64     `xorm:"not null default 0"`
 	Created      time.Time `xorm:"created"`
 	Updated      time.Time `xorm:"updated"`
 }
