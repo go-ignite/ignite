be70735b5c8920ae22cbc21a210449cc5e747648
Timothy <yexiaozhou2003@gmail.com>
2017-06-01 12:43:37 +0800

    update panel template, update user entity

--- models/user.go
+++ models/user.go
@@ -9,6 +9,8 @@ type User struct {
 	InviteCode   string
 	PackageLimit int       `xorm:"not null"` //Package bandwidth limit, unit: GB
 	PackageUsed  float32   //Package bandwidth used, unit: byte
+	ServicePort  int       `xorm:"not null default 0"` //Server port for SS
+	ServicePwd   string    //Password for SS
 	Created      time.Time `xorm:"created"`
 	Updated      time.Time `xorm:"updated"`
 }
---
+++ static/js/panel.js
@@ -0,0 +1,28 @@
+var User = function () {
+
+    var createHandler = function () {
+        $('#create-btn').on('click', function (e) {
+            e.preventDefault();
+            console.log("create-btn clicked...");
+
+            // var form = $('#signup-form');
+            // var url = form.attr("action");
+            // $.post(url, form.serialize(), function (data) {
+            //     if (data.success) {
+            //         window.location.href = '/panel/index';
+            //     } else {
+            //         //Signup failed
+            //         toastr.warning(data.message);
+            //         return false;
+            //     }
+            // }, "json");
+        });
+    };
+
+    return {
+        //main function to initiate the module
+        init: function () {
+            createHandler();
+        }
+    };
+}();
