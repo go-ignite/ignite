0b6410e9fbca31eb9f7a202578fbd1a5968a43bd
Timothy <yexiaozhou2003@gmail.com>
2017-06-01 14:00:21 +0800

    add service name field

--- models/user.go
+++ models/user.go
@@ -9,7 +9,8 @@ type User struct {
 	InviteCode   string
 	PackageLimit int       `xorm:"not null"` //Package bandwidth limit, unit: GB
 	PackageUsed  float32   //Package bandwidth used, unit: byte
-	ServicePort  int       `xorm:"not null default 0"` //Server port for SS
+	ServiceName  string    //Docker service name for SS
+	ServicePort  int       `xorm:"not null default 0"` //Docker service port for SS
 	ServicePwd   string    //Password for SS
 	Created      time.Time `xorm:"created"`
 	Updated      time.Time `xorm:"updated"`
