d3aa1d26b351eb1e68bca3d99af2e66e061feda5
Wendell Sun <iwendellsun@gmail.com>
2017-06-01 12:56:52 +0800

    Bug fix

--- static/js/user.js
+++ static/js/user.js
@@ -5,7 +5,9 @@ var User = function () {
             event.preventDefault();
             $('#hero').fadeOut(1000);
             $('#hero').css("display", "none");
-             $('#signup').fadeIn(500);
+
+            $('#login').css("display", "none")
+            $('#signup').fadeIn(500);
         });
 
 
@@ -29,7 +31,6 @@ var User = function () {
     var loginHandler = function () {
         $('.login').on('click', function (e) {
             e.preventDefault();
-
             $('#hero').fadeOut(1000);
             $('#hero').css("display", "none");
 
