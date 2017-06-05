e47ba2c970c663bf97cf24d182d8134e0474224d
Timothy <yexiaozhou2003@gmail.com>
2017-06-05 19:51:01 +0800

    adjust common button height

--- static/js/panel.js
+++ static/js/panel.js
@@ -7,7 +7,7 @@ var Panel = function () {
             console.log("create-btn clicked...");
 
             //1. Hide create-btn.
-            $('#create-btn').css('display', 'none');
+            $('#create-btn').fadeOut(500);
 
             //2. Show loading icon.
             $('#form-title').text('创建中...');
