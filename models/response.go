cfe4e49f319210b336466025a1ee9d016226656b
Timothy <yexiaozhou2003@gmail.com>
2017-06-06 11:32:19 +0800

    add service response struct

--- models/response.go
+++ models/response.go
@@ -5,3 +5,12 @@ type Response struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
 }
+
+//ServiceResponse struct for creating new SS service
+type ServiceResponse struct {
+	Success      bool   `json:"success"`
+	Message      string `json:"message"`
+	ServicePort  int    `json:"servicePort"`
+	ServicePwd   string `json:"servicePwd"`
+	PackageLimit int    `json:"packageLimit"`
+}
