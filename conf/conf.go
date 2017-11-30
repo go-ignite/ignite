8064bd93f0de9ece7d3ed0a39bceb63b2024b6cd
Wendell Sun <iwendellsun@gmail.com>
2017-11-30 13:26:27 +0800

    Read config options from os env

--- conf/conf.go
+++ conf/conf.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
 	toml "github.com/pelletier/go-toml"
 )
@@ -29,26 +30,45 @@ var (
 
 func InitConf() {
 	//Check config file
-	if _, err := os.Stat(*conf); os.IsNotExist(err) {
-		fmt.Println("Cannot load config.toml, file doesn't exist...")
-		os.Exit(1)
+	if _, err := os.Stat(*conf); os.IsExist(err) {
+		if config, err := toml.LoadFile(*conf); err == nil {
+			APP_Address = config.Get("app.address").(string)
+
+			SS_Image = config.Get("ss.image").(string)
+
+			HOST_Address = config.Get("host.address").(string)
+			HOST_From = int(config.Get("host.from").(int64))
+			HOST_To = int(config.Get("host.to").(int64))
+
+			DB_Driver = config.Get("db.driver").(string)
+			DB_Connect = config.Get("db.connect").(string)
+		}
 	}
-
-	config, err := toml.LoadFile(*conf)
-
-	if err != nil {
-		fmt.Println("Failed to load config file:", *conf)
-		fmt.Println(err.Error())
-		os.Exit(1)
+	if driver := os.Getenv("DB_DRIVER"); driver != "" {
+		DB_Driver = driver
 	}
-	APP_Address = config.Get("app.address").(string)
-
-	SS_Image = config.Get("ss.image").(string)
-
-	HOST_Address = config.Get("host.address").(string)
-	HOST_From = int(config.Get("host.from").(int64))
-	HOST_To = int(config.Get("host.to").(int64))
-
-	DB_Driver = config.Get("db.driver").(string)
-	DB_Connect = config.Get("db.connect").(string)
+	if connect := os.Getenv("DB_CONNECT"); connect != "" {
+		DB_Connect = connect
+	}
+	if image := os.Getenv("SS_IMAGE"); image != "" {
+		SS_Image = image
+	}
+	if address := os.Getenv("HOST_ADDRESS"); address != "" {
+		HOST_Address = address
+	}
+	if from := os.Getenv("HOST_FROM"); from != "" {
+		HOST_From, _ = strconv.Atoi(from)
+	}
+	if to := os.Getenv("HOST_To"); to != "" {
+		HOST_To, _ = strconv.Atoi(to)
+	}
+	fmt.Println("config: ", map[string]interface{}{
+		"address":      APP_Address,
+		"db_driver":    DB_Driver,
+		"db_connect":   DB_Connect,
+		"ss_image":     SS_Image,
+		"host_address": HOST_Address,
+		"host_from":    HOST_From,
+		"host_to":      HOST_To,
+	})
 }
