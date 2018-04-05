a87ea0ab9c6140ae75b08e11a70653c1e404f342
Wendell Sun <iwendellsun@gmail.com>
2018-04-05 22:39:46 +0800

    Add version flag
    fixup version flag

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-ignite/ignite/controllers"
 	"github.com/go-ignite/ignite/utils"
 )
 
-var confPath = flag.String("c", "./conf/config.toml", "config file")
+var (
+	confPath    = flag.String("c", "./conf/config.toml", "config file")
+	versionFlag = flag.Bool("v", false, "version")
+	version     = "unknown"
+)
 
 func main() {
 	flag.Parse()
+	if *versionFlag {
+		fmt.Println(version)
+		return
+	}
 	utils.InitConf(*confPath)
 	initRouter()
 }
