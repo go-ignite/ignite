22ffdccd654d846f3d72d22135f9415303f9eb00
Timothy <yexiaozhou2003@gmail.com>
2018-05-28 13:28:17 +0800

    update node model

--- db/node.go
+++ db/node.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Node struct {
-	Id        int64     `xorm:"pk autoincr notnull"`
-	Name      string    `xorm:"not null"`
-	Address   string    `xorm:"not null"`
-	Available bool      `xorm:"not null default 1"`
+	Id      int64  `xorm:"pk autoincr notnull"`
+	Name    string `xorm:"not null"`
+	Address string `xorm:"not null"`
+	// Available bool      `xorm:"not null default 1"` -- only in memory
 	Services  int       `xorm:"default 0"` // Number of running containers
 	Bandwidth float32   // total bandwidth used (for all containers), unit: GB
 	Created   time.Time `xorm:"created"`
-	Updated   time.Time `xorm:"updated"` // latest online time
+	// Updated   time.Time `xorm:"updated"` // latest online time -- only in memory
 }
