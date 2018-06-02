839429522d96a4ee1578a7486ab0ba5f0824538e
Wendell Sun <iwendellsun@gmail.com>
2018-06-02 22:04:32 +0800

    handler files rename

--- handler/admin.go
+++ handler/admin.go
@@ -1,14 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/go-ignite/ignite/config"
 	"github.com/go-ignite/ignite/models"
+	"github.com/go-ignite/ignite/utils"
 	"github.com/sirupsen/logrus"
 )
 
@@ -22,46 +20,30 @@ func NewAdminHandler(l *logrus.Logger) *AdminHandler {
 	}
 }
 
-func (router *AdminHandler) PanelLoginHandler(c *gin.Context) {
+func (ah *AdminHandler) PanelLoginHandler(c *gin.Context) {
 	loginEntity := struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
 	}{}
 
 	if err := c.BindJSON(&loginEntity); err != nil {
-		resp := models.Response{Success: false, Message: "Could not parse username & password..."}
-		c.JSON(http.StatusInternalServerError, &resp)
+		c.JSON(http.StatusInternalServerError, models.NewErrorResp("parse json data error"))
+		return
+	}
+	ah.WithField("loginEntity", loginEntity).Debug()
+
+	if loginEntity.Username != config.C.Admin.Username || loginEntity.Password != config.C.Admin.Password {
+		c.JSON(http.StatusOK, models.NewErrorResp("用户名或密码错误！"))
+		return
+	}
+	// Create the token
+	token, err := utils.CreateToken(config.C.Secret.Admin, -1)
+	if err != nil {
+		logrus.WithField("err", err).Error("generate token error")
+		c.JSON(http.StatusInternalServerError, models.NewErrorResp("登录失败！"))
 		return
 	}
 
-	fmt.Println("username:", loginEntity.Username)
-	fmt.Println("pwd:", loginEntity.Password)
-
-	if loginEntity.Username == config.C.Admin.Username && loginEntity.Password == config.C.Admin.Password {
-		// Create the token
-		token := jwt.New(jwt.GetSigningMethod("HS256"))
-		// Set some claims
-		token.Claims = jwt.MapClaims{
-			"exp": time.Now().Add(time.Hour * 1).Unix(),
-		}
-		// Sign and get the complete encoded token as a string
-		tokenString, err := token.SignedString([]byte(config.C.Secret.Admin))
-		resp := models.Response{}
-
-		if err != nil {
-			resp.Success = false
-			resp.Message = "Could not generate token"
-			c.JSON(http.StatusInternalServerError, &resp)
-			return
-		}
-
-		resp.Success = true
-		resp.Message = "success"
-		resp.Data = tokenString
-		c.JSON(http.StatusOK, &resp)
-		return
-	}
-
-	resp := models.Response{Success: false, Message: "Username of password is wrong!"}
-	c.JSON(http.StatusOK, &resp)
+	c.JSON(http.StatusOK, models.NewSuccessResp(token))
+	return
 }
