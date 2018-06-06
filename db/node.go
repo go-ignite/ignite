5dcab37235dd7c7180915bbfdbfad87ba59a48bb
Wendell Sun <iwendellsun@gmail.com>
2018-06-07 01:30:34 +0800

    Add comment field, make name unique

--- db/node.go
+++ db/node.go
@@ -6,8 +6,9 @@ import (
 
 type Node struct {
 	Id        int64     `xorm:"pk autoincr notnull"`
-	Name      string    `xorm:"not null"`
-	Address   string    `xorm:"not null"`
+	Name      string    `xorm:"notnull unique"`
+	Comment   string    `xorm:"notnull"`
+	Address   string    `xorm:"notnull"`
 	Services  int       `xorm:"default 0"` // Number of running containers
 	Bandwidth float32   // total bandwidth used (for all containers), unit: GB
 	Created   time.Time `xorm:"created"`
@@ -23,7 +24,7 @@ func UpsertNode(node *Node) (int64, error) {
 	if node.Id == 0 {
 		return engine.Insert(node)
 	}
-	return engine.Id(node.Id).Cols("name").Update(node)
+	return engine.Id(node.Id).Cols("name", "comment").Update(node)
 }
 
 func DeleteNode(id int64) (int64, error) {
